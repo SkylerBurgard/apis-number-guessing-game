86870da5891f2e991c3991ec3c2419d5587d05b2
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-02 10:26:17 -0500

    NPM install complete/ adding code comments for TODO list

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,13 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  //TODO: listen for submit of form
 }
+
+//TODO: handle form submit
+
+//TODO: send submitted guess to server
+
+
+//TODO: get all guess from server
